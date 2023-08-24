97c4003734e2a267e1dd4dfa126bcd2a1d27cc2a
yagasta <yashgajbhiye187@gmail.com>
2023-08-25 03:53:03 +0530

    stripe integrated

--- backend/stripeBackend/server.js
+++ backend/stripeBackend/server.js
@@ -1,17 +1,22 @@
-const epxress = require("express");
+const express = require("express");
 const { sanityClient } = require("./sanityClient");
 const cors = require("cors");
 
-const stripe = require("stripe")(process.env.VITE_SECRET_KEY);
+const stripe = require("stripe")(
+  "sk_test_51NiKdfSHs3QBZavVWoKSCRCZqJfTkzHClVnfvxMcxeqrvDbyDkrFy141iHBiD8ytaBxMNeTIldjISr6dQEULj90Y00sS9T9UtD"
+);
+
 const validateCartItems =
   require("use-shopping-cart/utilities").validateCartItems;
 
-const app = epxress();
-app.use(epxress.json());
+const app = express();
+app.use(express.json());
+
+const YOUR_DOMAIN = "http://127.0.0.1:5173";
 
 app.use(
   cors({
-    origin: "http://127.0.0.1:5173",
+    origin: YOUR_DOMAIN,
   })
 );
 
@@ -25,20 +30,29 @@ app.post("/checkout", async (req, res) => {
     let sanityData = await sanityClient.fetch(
       '*[_type in ["games", "gears"]]{title, "id": _id, price, "poster": poster.asset->url }'
     );
-    console.log(sanityData);
 
     let line_items = validateCartItems(sanityData, cartItems);
 
-    const session = await stripe.checkout.sessions.create({
+    console.log(line_items);
+
+    const params = {
+      submit_type: "pay",
+      mode: "payment",
       payment_method_types: ["card"],
-      success_url: `https://useshoppingcart.com/success.html`,
-      cancel_url: process.env.URL,
+      billing_address_collection: "auto",
+      shipping_address_collection: {
+        allowed_countries: ["US", "CA"],
+      },
       line_items,
-    });
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ sessionId: session.id }),
+      success_url: `http://127.0.0.1:5173/`,
+      cancel_url: `http://127.0.0.1:5173/`,
     };
+
+    const session = await stripe.checkout.sessions.create(params);
+
+    console.log(session);
+
+    res.status(200).json({ sessionId: session.id });
   } catch (error) {
     return {
       statusCode: 500,
